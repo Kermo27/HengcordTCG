e7c63e60402f898e3b143f34ef6799824937d200
Kermo <kacper.juszkiewicz7@gmail.com>
2026-02-16 18:18:48 +0100

    Setup: Radzen + Tailwind + FontAwesome + custom components

--- HengcordTCG.Blazor/HengcordTCG.Blazor.Client/Program.cs
+++ HengcordTCG.Blazor/HengcordTCG.Blazor.Client/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-// Configure HttpClient for API communication
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7156";
 
 builder.Services.AddScoped(sp =>
@@ -18,7 +17,6 @@ builder.Services.AddScoped(sp =>
     return http;
 });
 
-// Register Auth State Provider
 builder.Services.AddScoped<AuthStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<AuthStateProvider>());
 builder.Services.AddAuthorizationCore(options =>
@@ -27,16 +25,11 @@ builder.Services.AddAuthorizationCore(options =>
     options.AddPolicy(AuthPolicy.User, policy => policy.RequireAuthenticatedUser());
 });
 
-// Register HengcordTCGClient for API access
 builder.Services.AddScoped<HengcordTCGClient>();
-
-// Register Auth Service
 builder.Services.AddScoped<AuthService>();
-
-// Register Wiki Service
 builder.Services.AddScoped<WikiService>();
 
-// Radzen Blazor
+// Radzen
 builder.Services.AddRadzenDialog();
 builder.Services.AddRadzenNotification();
 builder.Services.AddRadzenTooltip();
